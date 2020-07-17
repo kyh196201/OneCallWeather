18f4053ce18894e1ab5c64187c8889e9de894175
kyh196201 <kyh196201@gmail.com>
2020-07-17 11:21:02 +0900

    swiper cdn파일 수정 및 index.html 변경

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,37 +1,37 @@
-import Swiper from "https://unpkg.com/swiper/js/swiper.esm.browser.bundle.min.js";
-import { $, createElement } from "../utils/helpers.js";
-import { removeFirstLoader, storage } from "../utils/functions.js";
-import ErrorSection from "./ErrorSection.js";
-import Menu from "./Menu.js";
-import CardContainer from "./CardContainer.js";
-import Settings from "./Settings.js";
-import SearchSection from "./SearchSection.js";
-import { SHOWING_CLASS } from "../utils/constants.js";
-import { api, getCoords } from "../utils/api.js";
-import DataLoading from "./DataLoading.js";
+// import Swiper from "https://unpkg.com/swiper/js/swiper.esm.browser.bundle.min.js";
+import { $, createElement } from "../utils/helpers.js"
+import { removeFirstLoader, storage } from "../utils/functions.js"
+import ErrorSection from "./ErrorSection.js"
+import Menu from "./Menu.js"
+import CardContainer from "./CardContainer.js"
+import Settings from "./Settings.js"
+import SearchSection from "./SearchSection.js"
+import { SHOWING_CLASS } from "../utils/constants.js"
+import { api, getCoords } from "../utils/api.js"
+import DataLoading from "./DataLoading.js"
 import {
   searchEvent,
   settingsEvent,
   errorEvent,
-} from "../utils/componentDealFunc.js";
+} from "../utils/componentDealFunc.js"
 
 class App {
   constructor({ $target }) {
-    console.log("App Starts");
+    console.log("App Starts")
     // Remove First Loading Page After Loading Compeleted
     document.onreadystatechange = () => {
-      removeFirstLoader();
-    };
+      removeFirstLoader()
+    }
 
-    this.$target = $target;
+    this.$target = $target
     this.state = {
       data: [],
       historyData: [],
       coord: [],
-    };
+    }
 
-    this.storage = storage;
-    this._dbName = "data";
+    this.storage = storage
+    this._dbName = "data"
 
     // ErrorSection
     this.errorSection = new ErrorSection({
@@ -41,7 +41,7 @@ class App {
         visible: false,
       },
       onClick: errorEvent.close.bind(this),
-    });
+    })
 
     try {
       //Menu
@@ -52,20 +52,20 @@ class App {
           openSearch: searchEvent.open.bind(this),
           openSetting: settingsEvent.open.bind(this),
         },
-      });
+      })
 
       // Card Container
       this.cardContainer = new CardContainer({
         $target: this.$target,
         data: [],
-      });
+      })
 
       // Settings
       this.settings = new Settings({
         $target: this.$target,
         visible: false,
         onClose: settingsEvent.close.bind(this),
-      });
+      })
 
       // Search Section
       this.searchSection = new SearchSection({
@@ -76,37 +76,37 @@ class App {
         onClose: searchEvent.close.bind(this),
         onClick: searchEvent.click.bind(this),
         onDelete: searchEvent.delete.bind(this),
-      });
+      })
 
       // Data Loading
       this.dataLoading = new DataLoading({
         $target: this.$target,
         visible: false,
-      });
+      })
 
-      this.init();
+      this.init()
       // this.bindEvents();
     } catch (err) {
-      console.error(err);
-      errorEvent.open.bind(this)(err);
+      console.error(err)
+      errorEvent.open.bind(this)(err)
     }
   }
 
   init = async () => {
     try {
       // 로컬 스토리지로부터 데이터를 불러와야한다.
-      let newState = "";
-      const storedData = this.storage.getItem(this._dbName);
+      let newState = ""
+      const storedData = this.storage.getItem(this._dbName)
       if (storedData) {
-        console.log("저장된 데이터 있을 경우");
-        newState = storedData;
-        this.setState(newState);
-        return;
+        console.log("저장된 데이터 있을 경우")
+        newState = storedData
+        this.setState(newState)
+        return
       }
-      console.log("저장된 데이터 없을 경우");
-      const coords = await getCoords();
-      const data = await api.fetchWeatherByCoords(coords);
-      const { current, daily, coord } = data;
+      console.log("저장된 데이터 없을 경우")
+      const coords = await getCoords()
+      const data = await api.fetchWeatherByCoords(coords)
+      const { current, daily, coord } = data
       newState = {
         data: [
           ...this.state.data,
@@ -117,41 +117,41 @@ class App {
         ],
         historyData: [...this.state.historyData],
         coord: [...this.state.coord, coord],
-      };
-      this.setState(newState);
+      }
+      this.setState(newState)
     } catch (err) {
-      console.error(err);
-      errorEvent.close.bind(this)();
+      console.error(err)
+      errorEvent.close.bind(this)()
     }
-  };
+  }
 
   setState = (newState) => {
-    this.storage.setItem(this._dbName, newState);
-    this.state = newState;
-    this.render();
-  };
+    this.storage.setItem(this._dbName, newState)
+    this.state = newState
+    this.render()
+  }
 
   render = () => {
     // render Cards;
-    this.cardContainer.setState(this.state.data);
+    this.cardContainer.setState(this.state.data)
     this.searchSection.setState({
       historyData: this.state.historyData,
       visible: false,
-    });
-  };
+    })
+  }
 
   onSearch = async (cityName) => {
     try {
-      const _cityName = cityName.trim();
-      const nodelist = document.querySelectorAll(".card");
-      const list = Array.from(nodelist);
-      const findItem = list.find((li) => li.dataset.city === _cityName);
+      const _cityName = cityName.trim()
+      const nodelist = document.querySelectorAll(".card")
+      const list = Array.from(nodelist)
+      const findItem = list.find((li) => li.dataset.city === _cityName)
       if (findItem) {
-        alert("이미 페이지가 존재합니다. 다시검색해주세요");
+        alert("이미 페이지가 존재합니다. 다시검색해주세요")
       } else {
-        this.dataLoading.setState({ visible: true });
-        const result = await api.fetchWeatherByCity(_cityName);
-        const { current, daily, coord } = result;
+        this.dataLoading.setState({ visible: true })
+        const result = await api.fetchWeatherByCity(_cityName)
+        const { current, daily, coord } = result
         const newState = {
           data: [...this.state.data, { current, daily }],
           historyData: [
@@ -162,19 +162,19 @@ class App {
             ...this.state.historyData,
           ],
           coord: [...this.state.coord, coord],
-        };
-        console.log(newState);
-        this.setState(newState);
-        this.dataLoading.setState({ visible: false });
+        }
+        console.log(newState)
+        this.setState(newState)
+        this.dataLoading.setState({ visible: false })
       }
     } catch (err) {
-      console.error(err);
-      errorEvent.open.bind(this)(err);
-      this.dataLoading.setState({ visible: false });
+      console.error(err)
+      errorEvent.open.bind(this)(err)
+      this.dataLoading.setState({ visible: false })
     }
-  };
+  }
 
-  bindEvents = () => {};
+  bindEvents = () => {}
 }
 
-export default App;
+export default App
